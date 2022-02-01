bdd383cb1ff5c871355d7a66c0233282a0916c43
baldas1173 <baldas1173@uta.edu.ec>
2022-01-31 20:14:54 -0500

    cambios aldas

--- src/app/property/propiedad-detail/propiedad-detail.component.ts
+++ src/app/property/propiedad-detail/propiedad-detail.component.ts
@@ -25,10 +25,10 @@ export class PropiedadDetailComponent implements OnInit {
     this.getPropiedadById(this.propiedadId);
   }
   onSelectNext() {
-    //this.propiedadId += 1;
-    //this.router.navigate(['propiedad-detail', this.propiedadId]);
-    //this.getPropiedadById(this.propiedadId)
-    //.add((x:any)=>this.map.cambioVista(this.propiedad));
+    this.propiedadId += 1;
+    this.router.navigate(['propiedad-detail', this.propiedadId]);
+    this.getPropiedadById(this.propiedadId)
+    .add((x:any)=>this.map.cambioVista(this.propiedad));
   }
   getPropiedadById(id:number){
     return this.housingService.getPropiedadById(id).subscribe((propiedad:any)=>{
